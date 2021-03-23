0e4a41c90e864753031a81c088f2f20554797572
Gareth Griffiths <glas@gareth.cymru>
2021-03-23 12:41:40 +0000

    refactoring

--- angular-sandbox/src/app/components/generic-selector/generic-selector.component.ts
+++ angular-sandbox/src/app/components/generic-selector/generic-selector.component.ts
@@ -17,21 +17,6 @@ import { GenericSelectorItem } from './generic-selector-item';
   styleUrls: ['./generic-selector.component.scss'],
 })
 export class GenericSelectorComponent {
-  constructor() {
-    this.filteredItems = this.genericSelectorControl.valueChanges.pipe(
-      startWith(null),
-      map((item: string | null) =>
-        item
-          ? this._filter(item)
-          : this.items
-              .slice()
-              .filter(
-                (currentItem) => this.selectedItems.indexOf(currentItem) < 0
-              )
-      )
-    );
-  }
-
   @Input() itemNameSingular = 'Item';
   @Input() itemNamePlural = 'Items';
   @Input() chipColour = '#F07C00';
@@ -44,33 +29,136 @@ export class GenericSelectorComponent {
   selectedItemsListVisible = false;
   separatorKeysCodes: number[] = [ENTER, COMMA];
   genericSelectorControl = new FormControl();
+  // update properties when DOM changes
   @ViewChild('chipInput') chipInput: ElementRef<HTMLInputElement>;
-  @ViewChild(MatAutocompleteTrigger, { read: MatAutocompleteTrigger }) genericSelectorAutocomplete: MatAutocompleteTrigger;
+  @ViewChild(MatAutocompleteTrigger, { read: MatAutocompleteTrigger })
+  genericSelectorAutocomplete: MatAutocompleteTrigger;
 
-  addItem(event: MatChipInputEvent): void {
+  constructor() {
+    this.filteredItems = this.genericSelectorControl.valueChanges.pipe(
+      startWith(null),
+      map((item: string | null) =>
+        item
+          ? this._filter(item)
+          : this.items
+              .sort((itemOne, itemTwo) => {
+                // alphabetical order
+                return itemOne.longName.localeCompare(itemTwo.longName);
+              })
+              .slice()
+              .filter(
+                // don't suggest already selected items
+                (currentItem) => this.selectedItems.indexOf(currentItem) < 0
+              )
+      )
+    );
+  }
+
+  addClickedItem(event: MatAutocompleteSelectedEvent): void {
+    if (event == null || event.option == null || !event.option.value) {
+      return;
+    }
+
+    const clikedItem = event.option.value;
+    this.selectedItems.push(clikedItem);
+
+    // clear the chip input of text
+    this.chipInput.nativeElement.value = '';
+    this.genericSelectorControl.setValue(null);
+  }
+
+  addTypedItem(event: MatChipInputEvent): void {
     if (event == null || event.value == null) {
       return;
     }
 
-    const itemValue = event.value.toString().toLowerCase();
+    const typedItem = event.value.toString().toLowerCase();
+    this._addSelectedItem(typedItem);
 
-    this._add(itemValue);
-
+    // clear typed item
     const input = event.input;
     if (input) {
       input.value = '';
     }
-
     this.genericSelectorControl.setValue(null);
   }
 
-  removeItem(itemToRemove: GenericSelectorItem): void {
+  removeClickedItem(itemToRemove: GenericSelectorItem): void {
+    this._removeSelectedItem(itemToRemove);
+  }
+
+  onChipInputClick(event: { stopPropagation: () => void }): void {
+    // open autocomplete panel on click
+    event.stopPropagation();
+    if (this.genericSelectorAutocomplete) {
+      this.genericSelectorAutocomplete.openPanel();
+    }
+  }
+
+  toggleSelectedItemsListVisibility(): void {
+    // switch whether selected item drop down is visible
+    this.selectedItemsListVisible = this.selectedItemsListVisible
+      ? false
+      : true;
+  }
+
+  onSelectedItemsListSelectionChange(event: MatSelectionListChange): void {
+    // remove items if unchecked from selection list
+    if (event == null || event.option == null || !event.option.value) {
+      return;
+    }
+
+    const itemToRemove = event.option.value;
+    this._removeSelectedItem(itemToRemove);
+  }
+
+  paste(event: ClipboardEvent): void {
+    if (event == null) {
+      return;
+    }
+
+    event.preventDefault();
+    event.clipboardData
+      .getData('Text')
+      .split(/,/)
+      .forEach((value) => {
+        if (value.trim()) {
+          this._addSelectedItem(value.trim());
+        }
+      });
+  }
+
+  private _addSelectedItem(typedName: string): void {
+    if (!typedName) {
+      return;
+    }
+
+    let itemToFind = typedName.toString().toLowerCase();
+
+    // find item to add it
+    let itemToAdd = this.items.find(
+      (item) =>
+        item.longName.toString().toLowerCase() === itemToFind ||
+        item.shortName.toString().toLowerCase() === itemToFind
+    );
+
+    if (itemToAdd) {
+      this.selectedItems.push(itemToAdd);
+    }
+  }
+
+  private _removeSelectedItem(itemToRemove: GenericSelectorItem): void {
+    if (itemToRemove == null) {
+      return;
+    }
+
     const item = this.selectedItems.find(
       (currentItem) =>
         currentItem.shortName === itemToRemove.shortName &&
         currentItem.longName === itemToRemove.longName
     );
 
+    // find index of selected item to remove it.
     const itemIndex = this.selectedItems.indexOf(item);
 
     if (itemIndex >= 0) {
@@ -79,71 +167,19 @@ export class GenericSelectorComponent {
     }
   }
 
-  addSelectedItem(event: MatAutocompleteSelectedEvent): void {
-    const itemToAdd = event.option.value;
-    this.selectedItems.push(itemToAdd);
-    this.chipInput.nativeElement.value = '';
-    this.genericSelectorControl.setValue(null);
-  }
-
-  toggleSelectedItemsListVisibility(): void {
-    this.selectedItemsListVisible = this.selectedItemsListVisible
-      ? false
-      : true;
-  }
-
-  onChipInputClick(event: { stopPropagation: () => void }): void {
-    event.stopPropagation();
-    if (this.genericSelectorAutocomplete) {
-      this.genericSelectorAutocomplete.openPanel();
-    }
-  }
-
-  onSelectedItemsListSelectionChange(event: MatSelectionListChange): void {
-    const itemToRemove = event.option.value;
-    this.removeItem(itemToRemove);
-  }
-
-  paste(event: ClipboardEvent): void {
-    event.preventDefault();
-    event.clipboardData
-      .getData('Text')
-      .split(/,/)
-      .forEach((value) => {
-        if (value.trim()) {
-          this._add(value.trim());
-        }
-      });
-  }
-
   private _filter(value: string): GenericSelectorItem[] {
-    if (value == null) {
+    if (!value) {
       return;
     }
 
     const filterValue = value.toString().toLowerCase();
 
+    // filter by long and short name ignoring case
     return this.items.filter(
       (item) =>
         this.selectedItems.indexOf(item) < 0 &&
         (item.shortName.toString().toLowerCase().includes(filterValue) ||
           item.longName.toString().toLowerCase().includes(filterValue))
     );
-  }
-
-  private _add(itemName: string): void {
-    if (!itemName) {
-      return;
-    }
-
-    const itemToAdd = this.selectedItems.find(
-      (item) =>
-        item.shortName.toString().toLocaleLowerCase() === itemName ||
-        item.longName.toString().toLocaleLowerCase() === itemName
-    );
-
-    if (itemToAdd) {
-      this.selectedItems.push(itemToAdd);
-    }
   }
 }
